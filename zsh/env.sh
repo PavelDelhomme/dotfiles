bde93e2e54bac218adbbb9f8e2fc6016fb6733b3
Pactivisme <pavel@delhomme.ovh>
2025-01-31 17:12:00 +0100

    Sauvegarde automatique - ven. 31 janv. 2025 17:12:00 CET

--- zsh/env.sh
+++ zsh/env.sh
@@ -3,6 +3,7 @@ if [ -z "$PATH_ORIGINAL" ]; then
 fi
 
 # === Export des variables d'environnement ===
+
 # Java (pour Android Studio)
 #export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
 
@@ -15,38 +16,31 @@ CMDLINE_TOOLS="$ANDROID_HOME/cmdline-tools/latest/bin"
 PLATFORM_TOOLS="$ANDROID_HOME/platform-tools"
 ANDROID_TOOLS="$ANDROID_HOME/tools"
 
-mkdir -p "$CMDLINE_TOOLS"
-mkdir -p "$PLATFORM_TOOLS"
-mkdir -p "$ANDROID_TOOLS"
+# Crée les répertoires nécessaires s'ils n'existent pas
+mkdir -p "$CMDLINE_TOOLS" "$PLATFORM_TOOLS" "$ANDROID_TOOLS"
 
-
-# Ajout des chemins Android au PATH
-add_to_path "$CMDLINE_TOOLS"
-add_to_path "$PLATFORM_TOOLS"
 
 # Dart pub-cache
-export PUB_CACHE="$HOME/.pub-cache"
 PUB_FLUTTER="$HOME/.pub-cache"
 PUB_FLUTTER_BIN="$PUB_FLUTTER/bin"
+mkdir -p "$PUB_FLUTTER_BIN"
 export PUB_CACHE="$PUB_FLUTTER"
 
 # Emacs & Doom
 export EMACSDIR="$HOME/.emacs.d/bin"
 
+# .NET SDK
 export DOTNET_ROOT="/usr/share/dotnet"
 export MSBuildSDKsPath="$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks"
 
 # === Ajout des chemins au PATH ===
 if type add_to_path &> /dev/null; then
-    #add_to_path "$JAVA_HOME/bin"
     add_to_path "/usr/lib/jvm/java-17-openjdk/bin"
-    #add_to_path "$ANDROID_HOME/emulator"
-    add_to_path "$ANDROID_HOME/cmdline-tools/bin"
-    add_to_path "$ANDROID_HOME/platform-tools"
-    add_to_path "$ANDROID_HOME/tools"
-    add_to_path "$PUB_CACHE/bin"
+    add_to_path "$CMDLINE_TOOLS"
+    add_to_path "$PLATFORM_TOOLS"
+    add_to_path "$ANDROID_TOOLS"
+    add_to_path "$PUB_FLUTTER_BIN"
     add_to_path "/opt/flutter/bin"
-    #add_to_path "/usr/local/share/doom-emacs/bin"
     add_to_path "$EMACSDIR"
     add_to_path "$DOTNET_ROOT"
     add_to_path "$HOME/.dotnet/tools"
@@ -54,15 +48,14 @@ else
     echo "❌ La fonction add_to_path n'est pas disponible."
 fi
 
-export PATH="$PATH:$PATH_ORIGINAL"
-
-
 # Nettoyage du PATH
 if type clean_path &> /dev/null; then
 	clean_path
 else
 	echo "❌ La fonction clean_path n'est pas disponible."
 fi
+
+export PATH="$PATH:$PATH_ORIGINAL"
 
 # Affiche un message de confirmation
 echo "✔️  ~/dotfiles/zsh/env.sh chargé avec succès"
