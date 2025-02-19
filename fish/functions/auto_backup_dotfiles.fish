fee8f13ffa80c2e437f91cf535f99e9bfe8a2282
Pactivisme <pavel@delhomme.ovh>
2025-02-19 20:56:44 +0100

    Auto-commit: 2025-02-19 20:56:44

--- fish/functions/auto_backup_dotfiles.fish
+++ fish/functions/auto_backup_dotfiles.fish
@@ -7,7 +7,7 @@ function auto_backup_dotfiles
         # Il y a des changements, on les commit
         git add .
         git commit -m "Auto-commit: $(date '+%Y-%m-%d %H:%M:%S')"
-        git push origin main  # Assurez-vous que 'main' est le nom de votre branche principale
+        git push origin master  # Assurez-vous que 'main' est le nom de votre branche principale
         echo "Dotfiles sauvegardés et poussés vers le dépôt distant."
     else
         echo "Aucun changement détecté dans les dotfiles."
